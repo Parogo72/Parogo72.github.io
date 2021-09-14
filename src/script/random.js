ab350afa6282d15bed18e9773bfd9944baba9815
Parogo_72 <76404961+Parogo72@users.noreply.github.com>
2021-09-14 13:14:00 +0200

    Add files via upload

--- src/script/random.js
+++ src/script/random.js
@@ -91,6 +91,7 @@ function radioToggle(e) {
 function languageToggle(e) {
   let lang;
   let selector = document.getElementById("lang-selector")
+  if(["en","es","ca"].includes(e)) lang = e;
   switch (e.className ? e.className : e) {
     case 'Español':
       lang = "es";
