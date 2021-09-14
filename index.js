8f6ce8bf2cad61b82deabaa288987726ce771751
Parogo_72 <76404961+Parogo72@users.noreply.github.com>
2021-09-14 18:07:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.use(express.static('static'));
 
-app.listen(3000, () => console.log('server listen on port', 3000));
+app.listen(process.env.PORT || 3000, () => console.log('server listen on port', 3000));
