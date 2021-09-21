380c59e5e6e246914557b23979561a0742379255
vwu-0 <60898184+vwu-0@users.noreply.github.com>
2021-09-21 22:27:49 +0200

    no se algo :v
    xd

--- index.js
+++ index.js
@@ -35,6 +35,9 @@ app.set('view engine', 'ejs');
 
 app.disable('x-powered-by');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     const text = t.get('es');
     let data_obj = decode(req.query.data);
